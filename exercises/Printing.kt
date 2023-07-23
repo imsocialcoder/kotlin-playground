66491ed6e2047cd64c7b0880edaf610a1478f0ac
imsocialcoder <39344644+imsocialcoder@users.noreply.github.com>
2023-07-23 17:48:36 +0200

    create the first exercise.

---
+++ exercises/Printing.kt
@@ -0,0 +1,10 @@
+/*
+  In this example, the purpose is to print out "Mary is 20 years old"
+  We can access the variables by using $ as given below.
+*/
+
+fun main() {
+    val name = "Mary"
+    val age = 20
+    println("$name is $age years old")
+}
