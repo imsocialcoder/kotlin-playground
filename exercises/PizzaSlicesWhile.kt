4d95e364913d8e1c9392e983160493f62e258b1f
imsocialcoder <39344644+imsocialcoder@users.noreply.github.com>
2023-07-28 15:45:34 +0200

    create while loop

---
+++ exercises/PizzaSlicesWhile.kt
@@ -0,0 +1,11 @@
+fun main() {
+    var pizzaSlices = 0
+    // Start refactoring here
+    while (pizzaSlices < 7){
+        pizzaSlices++
+    	println("There's only $pizzaSlices slice/s of pizza :(")
+    }
+    pizzaSlices++
+    // End refactoring here
+    println("There are $pizzaSlices slices of pizza. Hooray! We have a whole pizza! :D")
+}
